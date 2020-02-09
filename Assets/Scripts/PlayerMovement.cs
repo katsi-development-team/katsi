7f541e945859855d92e6b26d7d9af87991ea15ee
Spencer Kasper <spencer.kasper@gmail.com>
2020-02-09 17:22:24 -0600

    Player Movement
    Player can move and set world gravity to 0.  Just using a random sprite and not changing sprite directtion
    
    https://trello.com/c/C1NPcSuq

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2 : MonoBehaviour
+{
+
+    public float moveSpeed = 3f;
+
+    public Rigidbody2D rb;
+
+    //Vector2 means it will store horizontal and vertical
+    Vector2 movement;
+    float angle;
+
+    /* Do NOT use the built-in update function for anythig physics related !!!!!
+     * Useful for registering input instead
+     */
+    void Update()
+    {
+
+        /* Will give either 1 or -1, depending on horizontal input
+         * Left = -1, Right = 1, Pressing nothing returns 0
+         */
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+
+    /* Executed on fixed timer, unlike Update() which is stuck to the client framerate
+     * Called 50 times a second
+     */
+    private void FixedUpdate()
+    {
+
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    
+    }
+}
